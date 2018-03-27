99677f11dbfd9f40761652e600605e7bc33ebb85
yasemincidem <yasemincidem@gmail.com>
2018-03-27 03:31:35 +0300

    some solutions are applied for android

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const Container = styled.View`
   overflow: hidden;
   align-self: center;
   width: 150px;
+  background-color: white;
 `;
 export const HighLightView = styled.View`
   position: absolute;
